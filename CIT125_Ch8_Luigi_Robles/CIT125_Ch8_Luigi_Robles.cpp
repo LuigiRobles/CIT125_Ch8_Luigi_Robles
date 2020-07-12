f4dc1c93b9262b215edbf06842ecd64a6041bd8a
LuigiRobles <luigi.robles0846@my.riohondo.edu>
2020-07-11 17:37:25 -0700

    Update CIT125_Ch8_Luigi_Robles.cpp

--- CIT125_Ch8_Luigi_Robles/CIT125_Ch8_Luigi_Robles.cpp
+++ CIT125_Ch8_Luigi_Robles/CIT125_Ch8_Luigi_Robles.cpp
@@ -1,16 +1,19 @@
 // CIT125_Ch8_Luigi_Robles.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // CIT125 Intro to C++ Luigi Robles
-// Ch8 Figure 8-8
-
+// 07-11-2020 Summer Term
+// Ch8 Figure 8-8 The Clock Program
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    //output part of IPO
 	cout << "Minutes	Seconds" << endl;
+    //outter for loop
 	for (int minutes = 0; minutes < 2; minutes += 1)
 	{
+        //nested for loop
 		for (int seconds = 0; seconds < 3; seconds += 1)
 			cout << "	" << minutes
 			<< "		" << seconds << endl;
@@ -19,4 +22,3 @@ int main()
 	} // end for 
 	return 0;
 } // end of main function
-
