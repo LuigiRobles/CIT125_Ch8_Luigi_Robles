51c9b386ec43291d889c02a80bfe71783d0ac99a
LuigiRobles <luigi.robles0846@my.riohondo.edu>
2020-07-11 17:38:36 -0700

    Update CIT125_Ch8_Fig_8-11.cpp

--- CIT125_Ch8_Fig_8-11/CIT125_Ch8_Fig_8-11.cpp
+++ CIT125_Ch8_Fig_8-11/CIT125_Ch8_Fig_8-11.cpp
@@ -1,30 +1,38 @@
 // CIT125_Ch8_Fig_8-11.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // CIT125 Intro to C++ Luigi Robles
-// Ch8 Figure 8-11
+// 07-11-2020 Summer Term
+// Ch8 Figure 8-11 The Car Depreciation Program
 
 #include <iostream>
 using namespace std;
+#include <iomanip>
 
 int main()
 {
+    //declare and initialize variables
 	double originalValue = 0.0;
 	double depreciation = 0.0;
 	double currentValue = 0.0;
 
+    //input part of IPO
 	cout << "Original value: ";
 	cin >> originalValue;
-	cout << endl; << fixed << setprecision(0);
+	cout << endl << fixed << setprecision(0);
 
+    //output part of IPO
 	cout << "Depreciation rate: 15%" << endl;
 	cout << "Value after year: " << endl;
 	
+    //processing part of IPO
 	currentValue = originalValue;
-	for (int year = 1 < 6; year += 1)
-
-		depreciation = currentValue * 0.15 ;
-	currentValue -= depreciation;
-	cout << year << " $" << currentValue << endl;
-
+    //for loop to process depreciation for each year up to 5 years
+	for (int year = 1; year < 6; year += 1)
+	{
+		depreciation = currentValue * 0.15;
+		currentValue -= depreciation;
+		cout << year << " $" << currentValue << endl;
+	} // end for
 	return 0;
-}
+}	//end of main function
+
 
